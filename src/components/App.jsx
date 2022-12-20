a0e910d03f6ad65a8bf76719336e1fee3d5dc97f
VladFominov <111357638+VladFominov@users.noreply.github.com>
2022-12-20 15:17:36 +0200

    added a chart

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,5 @@
+
+
 export const App = () => {
   return (
     <div
@@ -11,6 +13,7 @@ export const App = () => {
       }}
     >
       Test
+      
     </div>
   );
 };
--- src/components/Chart/Chart.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Chart() {
-  return (
-    <div>My Chart</div>
-  )
-}
-
-export default Chart
