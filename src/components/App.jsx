1e09decf7c40f972b7abf1ecc9b2d33fdee4d60f
Oleksii Onufriienko <o.alexey1202@gmail.com>
2022-12-21 01:03:52 +0200

    redy elements

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,21 @@
+import TransactionsListDesktop from "./TransactionsListDesktop/TransactionsListDesktop";
+import TransactionsListMobile from "./TransactionsListMobile/TransactionsListMobile";
+
 export const App = () => {
   return (
     <div
       style={{
         height: '100vh',
-        display: 'flex',
+        display: 'block',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
         color: '#010101',
+         backgroundColor: 'gray',
       }}
     >
-      Test
+      <TransactionsListDesktop />
+      <TransactionsListMobile/>
     </div>
   );
 };
