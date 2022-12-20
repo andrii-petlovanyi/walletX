410e475a5f5df8e0acb97bbac98d973ac569c9b0
Andrii Petlovanyi <33178699+andrii-petlovanyi@users.noreply.github.com>
2022-12-20 20:52:20 +0200

    added(Redux/Auth selector): added seelctor getUserName in redux/auth

--- src/redux/auth/auth-selectors.js
+++ src/redux/auth/auth-selectors.js
@@ -2,6 +2,8 @@ const getIsLoggedIn = state => state.auth.isLoggedIn;
 
 const getUser = state => state.auth.user;
 
+const getUserName = state => state.auth.user.name;
+
 const getToken = state => state.auth.token;
 
 const getIsRefreshingUser = state => state.auth.isRefreshingUser;
@@ -11,6 +13,7 @@ const getUserBalance = state => state.auth.user.balance;
 const authSelectors = {
   getIsLoggedIn,
   getUser,
+  getUserName,
   getIsRefreshingUser,
   getUserBalance,
   getToken,
