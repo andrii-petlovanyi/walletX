5a75645032e3029f6c7c7c7c2825e5504de86077
AndreyLytvynov <mrjastonehome@gmail.com>
2022-12-20 11:42:07 +0200

    fixed put

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Layout from 'components/Layout/Layout';
+import RegistrationPage from 'pages/RegistrationPage/RegistrationPage';
 
 export const App = () => {
   return (
     <>
-      <Routes>
+      {/* <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<div>Home</div>} />
           <Route path="diagram" element={<div>Statistics</div>} />
         </Route>
-      </Routes>
+      </Routes> */}
+      <RegistrationPage />
     </>
   );
 };
